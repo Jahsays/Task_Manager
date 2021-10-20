0bc7df102b22b7af8775afffb4bfbf67cd175309
Chiaka Williams <chiaka394@gmail.com>
2021-10-20 19:42:15 +0100

    created schema, connected database

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect')
+require('dotenv').config()
 
 app.use(express.json());
 
@@ -12,4 +14,15 @@ app.use('/api/v1/tasks', tasks);
 
 const port = 3000;
 
-app.listen(port, console.log(`app is where we want it to be @ port ${port}`));
+const start = async () => {
+    try {
+        await connectDB(process.env.db_URI);
+        console.log('database connected')
+        app.listen(port, console.log(`server is listening @ port ${port}`));
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start();
+// app.listen(port, console.log(`app is where we want it to be @ port ${port}`));
